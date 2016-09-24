21b00644d23c7dfcd1ac88792633fd00a35cdbbf
aryamihirsingh <arya.m.singh.10@gmail.com>
2016-09-24 15:04:51 +0530

    changes in svm.py

--- svm/svm.py
+++ svm/svm.py
@@ -5,35 +5,22 @@ import numpy as np
 from pykernels.regular import GeneralizedHistogramIntersection
 
 class SVM:
+#			    X = np.array([[1,1], [0,0], [1,0], [0,1]])
+#               y = np.array([1, 1, 0, 0])
 
-	def __init__(self):
-		X = np.array([[1,1], [0,0], [1,0], [0,1]])
-		y = np.array([1, 1, 0, 0])
-
-	def print(self):
-		print 'Classification:'
+	def train(self,data,classes):
+		print 'Training:'
 		for clf, name in [(SVC(kernel=GeneralizedHistogramIntersection(), C=1000), 'pykernel')]:
-			clf.fit(X, y)
+			clf.fit(data, classes)
 			print name
 			print clf
-			print 'Predictions:', clf.predict(X)
-			print 'Accuracy:', accuracy_score(clf.predict(X), y)
 			print
-# from sklearn.svm import SVC
-# from sklearn.metrics import accuracy_score
-# import numpy as np
-#
-# from pykernels.basic import RBF
-#
-# X = np.array([[1,1], [0,0], [1,0], [0,1]])
-# y = np.array([1, 1, 0, 0])
-#
-# print 'Testing XOR'
-#
-# for clf, name in [(SVC(kernel=RBF(), C=1000), 'pykernel'), (SVC(kernel='rbf', C=1000), 'sklearn')]:
-#     clf.fit(X, y)
-#     print name
-#     print clf
-#     print 'Predictions:', clf.predict(X)
-#     print 'Accuracy:', accuracy_score(clf.predict(X), y)
-#     print
+
+	def predict(self,p_data):
+		print 'Predictions:'
+		for clf, name in [(SVC(kernel=GeneralizedHistogramIntersection(), C=1000), 'pykernel')]:
+			print name
+			print clf
+			print 'Predictions:', clf.predict(data)
+			print 'Accuracy:', accuracy_score(clf.predict(data), classes)
+			print
