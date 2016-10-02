8ee9757c0667bfaa06e7596f50f0e32c220cf9d6
Shivam Kakkar <shivam543@gmail.com>
2016-10-02 19:06:58 +0530

    Added Cuda K-means output files

--- parser.py
+++ parser.py
@@ -5,13 +5,29 @@ def create_parser():
     arguments = [
         ['-i', '--input', "input filename", str, "metafile"],
         ['-d', '--descriptor', "Descriptor to use", str, "descriptor"],
-        ['-o', '--output', "Output filenam", str, "outputfile"],
+        ['-o', '--output', "Output filename", str, "outputfile"],
         #['-v', '--validate', "validation set", str, 'validation_set']
     ]
 
     parser = argparse.ArgumentParser(description="Helper Script")
     for arg in arguments:
         unix, gnu, desc, typename, dest = arg
+        parser.add_argument(unix, gnu, help=desc, type=typename, 
+                required=True, dest=dest)
+    return parser
+
+def create_parser_cuda():
+    arguments = [ 
+        ['-i', '--input', "input filename", str, "metafile"],
+        ['-d', '--descriptor', "Descriptor to use", str, "descriptor"],
+        ['-o', '--output', "Output filename", str, "outputfile"],
+        ['-c', '--cudaoutfile', "Data output for Cuda", str, "cudaout"],
+        #['-v', '--validate', "validation set", str, 'validation_set']
+    ]   
+
+    parser = argparse.ArgumentParser(description="Helper Script")
+    for arg in arguments:
+        unix, gnu, desc, typename, dest = arg 
         parser.add_argument(unix, gnu, help=desc, type=typename, 
                 required=True, dest=dest)
     return parser
