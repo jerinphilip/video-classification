f3900082ea30761bb8079e4f0fa6a04e176012e2
Shivam Kakkar <shivam543@gmail.com>
2016-10-24 23:38:39 +0530

    Pre-merge

--- project_kmeans.py
+++ project_kmeans.py
@@ -1,13 +1,26 @@
 import bow.dataset_utils as du
 import bow.cluster_utils as cu
 from parser import create_parser_kmeans
+from functools import partial
+
+def generate_histogram(kmeans, descriptors):
+    r, c = kmeans.shape
+    print("Computing")
+    #print("Shape ",r,c)
+    vecs = [0 for i in range(r)]
+    for i, vec in enumerate(descriptors):
+        vecs[cu.find_nearest(kmeans,vec)] += 1;
+    return vecs
+
 
 if __name__ == '__main__':
     parser = create_parser_kmeans()
     args = parser.parse_args()
     print("Reading data...",)
     data = du.read_data(args.metafile, args.descriptor, 1)
-    print(data)
     print("Done.")
     _class, descriptors = zip(*data)
     means = du.read_kmeans(args.kmeans)
+    f = partial(generate_histogram, means)
+    rep = list(map(f, descriptors))
+    print(rep)
